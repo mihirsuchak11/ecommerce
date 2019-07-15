20349801b98917b9836f5d7d2c4789adb1149b23
Mihir Suchak <mihir.suchak11@gmail.com>
2019-07-15 22:32:07 +0530

    broken homepage into smaller components, finished styles of meny items

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import HomePage from './components/HomePage.jsx';
+import HomePage from './pages/HomePage';
 
 import './App.css';
 
 function App () {
     return (
         <HomePage />
-        
     );
 }
 
