80e700a4fadade4570546bf9618a7b0c28a670e5
Mihir Suchak <mihir.suchak11@gmail.com>
2019-07-17 12:22:16 +0530

    adding header component and links to app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
-import { Route, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import ShopPage from './pages/ShopPage/ShopPage';
+import Header from './components/Header/Header';
 
 import './App.css';
 
 function App () {
     return (
         <div>
+            <Header />
             <Route exact path="/" component={HomePage} />
             <Route exact path="/shop" component={ShopPage} />
         </div>
