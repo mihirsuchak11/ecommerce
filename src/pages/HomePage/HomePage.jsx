ef84351be3be7518ce87a8383a3526fc561a0a89
Mihir <mihir.suchak11@gmail.com>
2020-03-04 15:43:21 +0530

    hide directory component

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Directory from '../../components/Directory/Directory';
+// import Directory from '../../components/Directory/Directory';
 import './homepage.scss';
 
 const HomePage = () => (
     <div className='homepage'>
         <h1>Welcome to my Homepage</h1>
-        <Directory />
+        {/* <Directory /> */}
     </div>
 );
 
